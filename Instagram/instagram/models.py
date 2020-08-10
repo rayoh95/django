f51c3528141f16e174c6a3150e2c3acfd7839eb4
오승환 <rayoh95@naver.com>
2020-08-10 13:19:32 +0900

    * model 생성 및 migrations 완료 * 기본 url 설정 완료

--- Instagram/instagram/models.py
+++ Instagram/instagram/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length = 100)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete = models.CASCADE)
-    views = models.IntegerField(default = 0)
+    like = models.IntegerField(default = 0)
     image = models.ImageField(null=True, blank=True, upload_to="image")
 
 class Comment(models.Model):
